4ae17a3aae1951d419dafc937fc5a1a13fda6f28
bhagy <bhagyparmar727@gmail.com>
2025-04-02 12:39:49 +0530

    Update tetris.cpp

--- tetris.cpp
+++ tetris.cpp
@@ -22,7 +22,6 @@ std::vector<std::vector<bool>> board(BOARD_HEIGHT, std::vector<bool>(BOARD_WIDTH
 Tetromino currentPiece;
 int score = 0;
 
-// Hide the cursor
 void hideCursor() {
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursorInfo;
@@ -31,22 +30,20 @@ void hideCursor() {
     SetConsoleCursorInfo(out, &cursorInfo);
 }
 
-// Set console color
 void setColor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-// Get color for each tetromino type
 int getColorForPiece(TetrominoType type) {
     switch (type) {
-        case TetrominoType::I: return 11; // Cyan
-        case TetrominoType::J: return  9; // Blue
-        case TetrominoType::L: return  6; // Orange
-        case TetrominoType::O: return 14; // Yellow
-        case TetrominoType::S: return 10; // Green
-        case TetrominoType::T: return 13; // Purple
-        case TetrominoType::Z: return 12; // Red
-        default: return 15; // White
+        case TetrominoType::I: return 11;
+        case TetrominoType::J: return  9;
+        case TetrominoType::L: return  6;
+        case TetrominoType::O: return 14;
+        case TetrominoType::S: return 10;
+        case TetrominoType::T: return 13;
+        case TetrominoType::Z: return 12;
+        default: return 15;
     }
 }
 
@@ -150,17 +147,28 @@ void rotatePiece() {
     }
 }
 
+void hardDrop() {
+    while (true) {
+        currentPiece.y++;
+        if (isCollision()) {
+            currentPiece.y--;
+            mergePieceToBoard();
+            clearLines();
+            spawnNewPiece();
+            break;
+        }
+    }
+}
+
 void drawBoard() {
-    // Move cursor to top-left instead of clearing screen to reduce flickering
     COORD cursorPos = {0, 0};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPos);
     
     std::cout << "Score: " << score << "\n";
-    // Top border
     std::cout << " " << std::string(BOARD_WIDTH * 2, '_') << " \n";
 
     for (int y = 0; y < BOARD_HEIGHT; ++y) {
-        std::cout << "|"; // Left border
+        std::cout << "|";
         for (int x = 0; x < BOARD_WIDTH; ++x) {
             bool isCurrent = false;
             for (size_t pieceY = 0; pieceY < currentPiece.shape.size(); ++pieceY) {
@@ -176,21 +184,19 @@ void drawBoard() {
             if (isCurrent) {
                 setColor(getColorForPiece(currentPiece.type));
                 std::cout << "[]";
-                setColor(15); // Reset to white
+                setColor(15);
             } else if (board[y][x]) {
-                // For placed pieces, use a darker color
-                setColor(8); // Gray
+                setColor(8);
                 std::cout << "##";
-                setColor(15); // Reset to white
+                setColor(15);
             } else {
                 std::cout << "  ";
             }
         }
-        std::cout << "|\n"; // Right border
+        std::cout << "|\n";
     }
-    // Bottom border
     std::cout << "+" << std::string(BOARD_WIDTH * 2, '-') << "+\n";
-    std::cout << "Controls: WASD to move, W to rotate, Q to quit\n";
+    std::cout << "Controls: WASD to move, W to rotate, SPACE to hard drop, Q to quit\n";
 }
 
 void gameLoop() {
@@ -211,18 +217,19 @@ void gameLoop() {
                         clearLines();
                         spawnNewPiece();
                         if (isCollision()) {
-                            setColor(12); // Red
+                            setColor(12);
                             std::cout << "Game Over! Final Score: " << score << "\n";
-                            setColor(15); // White
+                            setColor(15);
                             return;
                         }
                     }
                     break;
                 case 'w': rotatePiece(); break;
+                case ' ': hardDrop(); break;  // Spacebar for hard drop
                 case 'q': 
-                    setColor(14); // Yellow
+                    setColor(14);
                     std::cout << "Game Over! Final Score: " << score << "\n";
-                    setColor(15); // White
+                    setColor(15);
                     return;
             }
         }
@@ -235,9 +242,9 @@ void gameLoop() {
                 clearLines();
                 spawnNewPiece();
                 if (isCollision()) {
-                    setColor(12); // Red
+                    setColor(12);
                     std::cout << "Game Over! Final Score: " << score << "\n";
-                    setColor(15); // White
+                    setColor(15);
                     return;
                 }
             }
